4795e0ff65bb9ea77e8a62bd7be7302ba43f840b
킹짱규 <106237347+Kyukyu100@users.noreply.github.com>
2023-10-19 16:17:01 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException  {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String str=  bf.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		double a = Integer.parseInt(st.nextToken());
+		double b = Integer.parseInt(st.nextToken());
+		System.out.println(a/b);
+	}
+}
