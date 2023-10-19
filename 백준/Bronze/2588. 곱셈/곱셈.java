d4d58ef03b1d4120ac9434030b8d994e476301b3
킹짱규 <106237347+Kyukyu100@users.noreply.github.com>
2023-10-19 17:06:03 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException  {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(bf.readLine());
+		int b = Integer.parseInt(bf.readLine());
+		System.out.println(a * (b % 10) );
+		System.out.println(a * (( b % 100) / 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a*b);
+	}
+}
