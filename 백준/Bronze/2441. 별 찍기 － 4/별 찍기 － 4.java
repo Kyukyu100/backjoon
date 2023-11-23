387014aa669d26685cd29fcc9642d54e41f411db
킹짱규 <106237347+Kyukyu100@users.noreply.github.com>
2023-11-23 23:51:56 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 300 ms, Memory: 19824 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(bf.readLine());
+		for(int i=0; i<x; i++) {
+			for(int y=0; y<i; y++) {
+				System.out.print(" ");
+			}
+			for(int z=x-1; z>=i; z--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		
+	}	
+	
+}
