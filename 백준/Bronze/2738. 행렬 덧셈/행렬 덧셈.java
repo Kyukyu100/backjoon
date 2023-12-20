ce38396af043c2fc72aa78c44cd6a9c6b06bd6dd
킹짱규 <kjkjk0@naver.com>
2023-12-20 22:55:16 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 216 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+
+
+
+    }
+
+}
