723b81053e042ebf7cb48b410b4a4bf4e115bc91
킹짱규 <kjkjk0@naver.com>
2024-01-08 23:12:43 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int count = 0;
+		int result = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			if(N % i == 0) {
+				count++;
+			}
+			if(count == K) {
+				result = i;
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
