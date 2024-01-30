1c706b75bf6ac06af85a40e74d1f3ab3f9056bc9
킹짱규 <kjkjk0@naver.com>
2024-01-30 23:24:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 140 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+        br.close();
+
+        bw.write(n*n*n + "\n" + 3);
+        bw.flush();
+        bw.close();
+    }
+}
