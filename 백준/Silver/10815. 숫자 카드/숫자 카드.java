3f53e7101f32508b6a3c160d396fd6101737bd38
킹짱규 <kjkjk0@naver.com>
2024-02-22 23:58:15 +0900

    [Silver V] Title: 숫자 카드, Time: 928 ms, Memory: 121356 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,47 +1,33 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n, m;
-    static int arr[];
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
+        int n, m;
+        HashMap<String, Integer> owned = new HashMap<>();
+
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for(int i = 0 ; i < n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-
-        Arrays.sort(arr);
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            owned.put(st.nextToken(), 0);
+        }
 
         m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
-        for(int i = 0 ; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if(binarySearch(num)) bw.write("1 ");
-            else bw.write("0 ");
+        for (int i=0; i<m; i++) {
+            if (owned.get(st.nextToken()) != null) {
+                sb.append("1 ");
+            } else {
+                sb.append("0 ");
+            }
         }
-
-        bw.close();
-        br.close();
-    }
-    private static boolean binarySearch(int num) {
-        int leftIndex = 0;
-        int rightIndex = n - 1;
-
-        while(leftIndex <= rightIndex){
-            int midIndex = (leftIndex + rightIndex) / 2;
-            int mid = arr[midIndex];
-
-            if(num < mid) rightIndex = midIndex - 1;
-            else if(num > mid) leftIndex = midIndex + 1;
-            else return true;
-        }
-        return false;
+        System.out.println(sb.toString());
     }
 }
