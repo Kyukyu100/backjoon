63ea40336b8a44d7c2612e9b5667311f5b0faa0b
킹짱규 <kjkjk0@naver.com>
2024-04-25 23:59:12 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 572 ms, Memory: 54880 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+ 
+		bw.write((int) (Math.pow(2, N) - 1) + "\n");
+ 
+		Hanoi(N, 1, 2, 3);
+		bw.flush();
+		bw.close();
+ 
+	}
+ 
+	public static void Hanoi(int N, int start, int mid, int to) throws IOException {
+		if (N == 1) {
+			bw.write(start + " " + to + "\n");
+			return;
+		}
+ 
+		Hanoi(N - 1, start, to, mid);
+    
+		bw.write(start + " " + to + "\n");
+    
+		Hanoi(N - 1, mid, start, to);
+ 
+	}
+}
