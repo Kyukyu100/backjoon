ac6c99a491bb88556ed2dd4da86f641663f6af38
킹짱규 <kjkjk0@naver.com>
2024-05-03 00:45:36 +0900

    [Gold IV] Title: 스도쿠, Time: 464 ms, Memory: 25528 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static int[][] arr = new int[9][9];
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		for (int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+ 
+		sudoku(0, 0);
+	}
+ 
+	public static void sudoku(int row, int col) {
+		if (col == 9) {
+			sudoku(row + 1, 0);
+			return;
+		}
+ 
+		if (row == 9) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(arr[i][j]).append(' ');
+				}
+				sb.append('\n');
+			}
+			System.out.println(sb);
+			System.exit(0);
+		}
+ 
+		if (arr[row][col] == 0) {
+			for (int i = 1; i <= 9; i++) {
+				// i 값이 중복되지 않는지 검사
+				if (possibility(row, col, i)) {
+					arr[row][col] = i;
+					sudoku(row, col + 1);
+				}
+			}
+			arr[row][col] = 0;
+			return;
+		}
+ 
+		sudoku(row, col + 1);
+	}
+ 
+	public static boolean possibility(int row, int col, int value) {
+		for (int i = 0; i < 9; i++) {
+			if (arr[row][i] == value) {
+				return false;
+			}
+		}
+ 
+		for (int i = 0; i < 9; i++) {
+			if (arr[i][col] == value) {
+				return false;
+			}
+		}
+ 
+		int set_row = (row / 3) * 3; 
+		int set_col = (col / 3) * 3; 
+ 
+		for (int i = set_row; i < set_row + 3; i++) {
+			for (int j = set_col; j < set_col + 3; j++) {
+				if (arr[i][j] == value) {
+					return false;
+				}
+			}
+		}
+ 
+		return true; 
+	}
+ 
+}
