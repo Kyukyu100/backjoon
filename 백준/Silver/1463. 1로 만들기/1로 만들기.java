3e031a6b72902858dd2c8d7a5664ee8fa1ac96be
킹짱규 <kjkjk0@naver.com>
2024-05-20 23:50:46 +0900

    [Silver III] Title: 1로 만들기, Time: 256 ms, Memory: 66336 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	static Integer[] dp;
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		dp = new Integer[N + 1];
+		dp[0] = dp[1] = 0;
+ 
+		System.out.print(recur(N));
+ 
+	}
+ 
+	static int recur(int N) {
+ 
+		if (dp[N] == null) {
+			if (N % 6 == 0) {
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+			}
+			else if (N % 3 == 0) {
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+			}
+			else if (N % 2 == 0) {
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+			}
+			else {
+				dp[N] = recur(N - 1) + 1;
+			}
+		}
+		return dp[N];
+	}
+ 
+}
